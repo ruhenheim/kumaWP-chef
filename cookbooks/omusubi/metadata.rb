52b4fcf0bf6a21102f517db067c2fec42f725d62
uchiyama <asd.uchiyama@gmail.com>
2013-08-24 12:22:10 +0900

    omusubi をちょとfork+customeしました。

---
+++ cookbooks/omusubi/metadata.rb
@@ -0,0 +1,9 @@
+initial creator  "Yusuke Ando"
+maintainer_email "yandod@hoge.jp"
+license          "none"
+description      ""
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+depends          "database"
+# depends          "apt"
